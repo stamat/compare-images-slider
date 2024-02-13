d6ee9f6ee62a3150b9ee3818966c1c3a9bcc613b
Nikola Stamatovic <stamatmail@gmail.com>
2024-02-13 01:21:40 +0100

    some very old code...

--- src/markup/index.md
+++ src/markup/index.md
@@ -4,6 +4,21 @@ layout: default
 <div class="container">
   <h1>{{ site.title }}</h1>
   {{ site.description }}
+</div>
 
-  Discover [Poops](https://github.com/stamat/poops/) - super fast and easy bundler and static site generator.
+<div class="before-after-wrap">
+  <img class="spacer main-spacer" src="{{'switch-spacer.png' | asset_url}}" alt="" />
+  {% for block in section.blocks %}
+      <div class="before-after slider target-{{block.settings.type}}{%if forloop.index == 1%} active{% endif %}">
+            <div class="slide slide1 loadme" style="background-image: url({{block.settings.after | img_url: '300x'}})" data-full="{{block.settings.after | img_url: '1440x'}}"></div>
+            <div class="resize">
+                <div class="slide-wrap">
+                    <div class="slide slide2 loadme" style="background-image: url({{block.settings.before | img_url: '300x'}})" data-full="{{block.settings.before | img_url: '1440x'}}">
+                        <img class="spacer" src="{{'switch-spacer.png' | asset_url}}" alt="" />
+                    </div>
+                </div>
+            </div>
+            <span class="handle icon-drag"></span>
+      </div>
+  {% endfor %}
 </div>
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,60 @@
 import { isMobile } from 'book-of-spells'
 
-console.log(`hello ${isMobile() ? 'mobile' : 'desktop'} world!`)
+const drags = (dragElement, resizeElement, container) => {
+  const startDrag = (e) => {
+      const startX = e.pageX || e.touches[0].pageX;
+      const dragWidth = dragElement.offsetWidth;
+      const posX = dragElement.offsetLeft + dragWidth - startX;
+      const containerOffset = container.offsetLeft;
+      const containerWidth = container.offsetWidth;
+
+      const minLeft = containerOffset + 10;
+      const maxLeft = containerOffset + containerWidth - dragWidth - 10;
+
+      const doDrag = (e) => {
+          const moveX = e.pageX || e.touches[0].pageX;
+          let leftValue = moveX + posX - dragWidth;
+
+          leftValue = Math.max(Math.min(leftValue, maxLeft), minLeft);
+          const widthValue = ((leftValue + dragWidth / 2 - containerOffset) * 100 / containerWidth) + '%';
+
+          dragElement.style.left = widthValue;
+          resizeElement.style.width = widthValue;
+      }
+
+      const stopDrag = () => {
+          dragElement.classList.remove('ba-draggable');
+          resizeElement.classList.remove('ba-resizable');
+          document.removeEventListener('mousemove', doDrag);
+          document.removeEventListener('mouseup', stopDrag);
+          document.removeEventListener('touchmove', doDrag);
+          document.removeEventListener('touchend', stopDrag);
+      }
+
+      dragElement.classList.add('ba-draggable');
+      resizeElement.classList.add('ba-resizable');
+
+      document.addEventListener('mousemove', doDrag);
+      document.addEventListener('mouseup', stopDrag);
+      document.addEventListener('touchmove', doDrag);
+      document.addEventListener('touchend', stopDrag);
+
+      e.preventDefault();
+  }
+
+  dragElement.addEventListener('mousedown', startDrag);
+  dragElement.addEventListener('touchstart', startDrag);
+}
+
+Element.prototype.beforeAfter = function() {
+  const adjustSlider = () => {
+      const width = this.offsetWidth + 'px';
+      this.querySelector('.resize img').style.width = width;
+      this.querySelector('.resize .slide').style.width = width;
+  }
+
+  adjustSlider();
+  drags(this.querySelector('.handle'), this.querySelector('.resize'), this);
+
+  window.addEventListener('resize', adjustSlider);
+}
